34096b139cc13eecd994876baa6f90feaee0610a
Michael Verthin <michaelvn5202@gmail.com>
2022-07-31 23:17:53 +0300

    Reamerged new with old

--- Michael Website/js/scripts.js
+++ Michael Website/js/scripts.js
@@ -1,5 +1,4 @@
 window.addEventListener('DOMContentLoaded', event => {
-
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -11,7 +10,6 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
-
     };
 
     // Shrink the navbar 
@@ -62,15 +60,13 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
 var english = document.getElementById('en_click'),
     hebraw = document.getElementById('he_click'),
-    russian = document.getElementById('ru_click'),
     en_txt = document.querySelectorAll('#en'),
     he_txt = document.querySelectorAll('#he'),
-    ru_txt = document.querySelectorAll('#ru'),
     nb_en = en_txt.length,
     nb_he = he_txt.length,
-    nb_ru = ru_txt.length,
     light = document.getElementById('lightmode'),
     dark = document.getElementById('darkmode'),
     currentTheme = localStorage.getItem('data-theme') ? localStorage.getItem('data-theme') : null,
@@ -98,49 +94,34 @@ function bgcolor(colorOff, colorOn){
     }
 }
 
-
 english.addEventListener('click', function() {
-    langue(english,hebraw,russian);
+    langue(hebraw,english);
 }, false);
 
 hebraw.addEventListener('click', function() {
-    langue(hebraw,english,russian);
+    langue(english,hebraw);
 }, false);
-
-russian.addEventListener('click', function() {
-    langue(russian,hebraw,english);
-}, false);
-
-function langue(langue1,langue2,langue3){
-    if (langue2.classList.contains('current_lang')) {
-        langue1.classList.toggle('current_lang');
-        langue2.classList.toggle('current_lang');
+window.addEventListener('load',() => {
+    init();
+});
+function langue(langueOff,langueOn){
+    if(langueOff.classList.contains('currentLang')&&langueOn.innerHTML == 'he'){
+        console.log('he click')
+        afficher(he_txt, nb_he);
+        cacher(en_txt, nb_en);
+        hebraw.classList.toggle('currentLang');
+        english.classList.toggle('currentLang');
         document.documentElement.setAttribute('data-lang', 'he');
         localStorage.setItem('data-lang', 'he');
     }
-    if (langue3.classList.contains('current_lang')) {
-        langue1.classList.toggle('current_lang');
-        langue3.classList.toggle('current_lang');
-        document.documentElement.setAttribute('data-lang', 'ru');
-        localStorage.setItem('data-lang', 'ru');
-    }else{
-        document.documentElement.setAttribute('data-lang', 'en');
-        localStorage.setItem('data-lang', 'en');
-    }
-    if(langue1.innerHTML == 'en'){
+    else if(langueOff.classList.contains('currentLang')&&langueOn.innerHTML == 'en'){
+        console.log('en click')
         afficher(en_txt, nb_en);
         cacher(he_txt, nb_he);
-        cacher(ru_txt, nb_ru);
-    }
-    else if(langue1.innerHTML == 'he'){
-        afficher(he_txt, nb_he);
-        cacher(en_txt, nb_en);
-        cacher(ru_txt, nb_ru);
-    }
-    else if(langue1.innerHTML == 'ru'){
-        afficher(ru_txt, nb_ru);
-        cacher(en_txt, nb_en);
-        cacher(he_txt, nb_he);
+        hebraw.classList.toggle('currentLang');
+        english.classList.toggle('currentLang');
+        document.documentElement.setAttribute('data-lang', 'en');
+        localStorage.setItem('data-lang', 'en');
     }
 }
 function afficher(txt,nb){
@@ -154,20 +135,28 @@ function cacher(txt,nb){
     }
 }
 function init(){
-    langue(english,hebraw,russian);
-    /*if (currentlang) {
-        document.documentElement.setAttribute('data-lang', currentlang);
-        if (currentlang === 'he'){
-            hebraw.classList.toggle('current_lang');
-            english.classList.toggle('current_lang');
-            hebraw.classList.setItem('current_lang');
+    if(currentlang === 'he'){
+        console.log(currentlang);
+        if(!hebraw.classList.contains('currentLang')){
+            hebraw.classList.toggle('currentLang');
         }
-        if (currentlang === 'ru'){
-            russian.classList.toggle('current_lang');
-            english.classList.toggle('current_lang');
-            russian.classList.setItem('current_lang');
+        afficher(he_txt, nb_he);
+        cacher(en_txt, nb_en);
+        document.documentElement.setAttribute('data-lang', 'he');
+        localStorage.setItem('data-lang', 'he');
+    }
+    else if(currentlang === 'en'){
+        console.log(currentlang);
+        if(!english.classList.contains('currentLang')){
+            english.classList.toggle('currentLang');
         }
-    }*/
+        afficher(en_txt, nb_en);
+        cacher(he_txt, nb_he);
+        document.documentElement.setAttribute('data-lang', 'en');
+        localStorage.setItem('data-lang', 'en');
+    }
+}
+function initcolor(){
     if (currentTheme) {
         document.documentElement.setAttribute('data-theme', currentTheme);
         if (currentTheme === 'dark'){
@@ -175,8 +164,9 @@ function init(){
             light.classList.toggle('current_clr_light');
             dark.classList.setItem('current_clr_dark');
         }
-    }
+    pagechange(homelink,bloglink);
     bgcolor(dark,light);
-    // pagechange(homelink,bloglink);
+    }
 }
-init();
+initcolor();
+
